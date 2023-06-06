1e5656891d675355c7959dcb82dac793c20e05b3
Falconilham <ilhama36@gmail.com>
2023-06-06 17:32:45 +0700

    add cloud and certificate for MONGODB

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
 const DB_URI = 'mongodb://localhost:27017/mydatabase'; // Replace with your MongoDB connection URI
+const DB_URI_CLOUD = 'mongodb+srv://cluster0.wtt02mk.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority'
+
+const credentials = '../../cert/X509-cert-224455657921162270.pem'
 
 const connectDB = async () => {
+  const usedDB = process.env.NODE_ENV === "production" ? DB_URI_CLOUD : DB_URI
+  console.log({ usedDB })
   try {
-    await mongoose.connect(DB_URI, {
+    await mongoose.connect(usedDB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      sslKey: credentials,
+      sslCert: credentials,
     });
 
     console.log('Connected to the MongoDB database');
